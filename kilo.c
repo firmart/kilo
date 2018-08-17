86ca2da26695290a421325ede4d2178d5123c065
firmart <firminmartin24@gmail.com>
2018-08-17 14:55:57 +0200

    step 94: add filename as field to global config

--- kilo.c
+++ kilo.c
@@ -51,6 +51,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow* row;
+  char *filename;
   struct termios orig_termios;
 };
 
@@ -216,6 +217,9 @@ void editorAppendRow(char *s, size_t len) {
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+
   FILE *fp = fopen(filename, "r");
   if (!fp) die("fopen");
   char *line = NULL;
@@ -433,6 +437,7 @@ void initEditor() {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.filename = NULL;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
   E.screenrows -= 1;
