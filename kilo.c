44942609233baeaba5afd39ff8c41e574c71621e
firmart <firminmartin24@gmail.com>
2018-08-19 20:42:32 +0200

    step 146: call update syntax

--- kilo.c
+++ kilo.c
@@ -244,6 +244,8 @@ void editorUpdateRow(erow *row) {
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
@@ -269,7 +271,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
   free(row->render);
   free(row->chars);
-  free(row->hl)
+  free(row->hl);
 }
 
 void editorDelRow(int at) {
